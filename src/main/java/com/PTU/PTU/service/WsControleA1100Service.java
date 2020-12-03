545f84e0c6419fd59fd3d30065f6c296aa4e83f1
TEC- Antonio Marcos Porpeta <porpeta_antonio@unimedlondrina.com.br>
2020-12-03 15:21:23 -0300

    Criado endpoint buscando registros por data de inclusao

--- src/main/java/com/PTU/PTU/service/WsControleA1100Service.java
+++ src/main/java/com/PTU/PTU/service/WsControleA1100Service.java
@@ -1,13 +1,11 @@
 package com.PTU.PTU.service;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
