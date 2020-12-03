254fa854931998865941bf06b9caac1ae039ce4f
TEC- Antonio Marcos Porpeta <porpeta_antonio@unimedlondrina.com.br>
2020-12-03 15:20:38 -0300

    Criado endpoint buscando registros por data de inclusao

--- src/main/java/com/PTU/PTU/model/repository/WsControleA1100Repository.java
+++ src/main/java/com/PTU/PTU/model/repository/WsControleA1100Repository.java
@@ -1,12 +1,10 @@
 package com.PTU.PTU.model.repository;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import com.PTU.PTU.model.WsControleA1100;
 
